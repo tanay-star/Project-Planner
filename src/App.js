8d9a16890cd82193b066df8b5f8c63bc46ca537f
Tanay Banerjee <banerjeetanay333@gmail.com>
2020-09-03 06:26:18 -0700

    updated the components and added redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
       <Switch>
         <Route exact path='/' component={Dashboard}/>
         <Route exact path='/project/:id' component={ProjectDetails}/>
+        <Route exact path='/signin' component={SignIn} />
+        <Route exact path='/signup' component={SignUp} />
+        <Route exact path='/create' component={CreateProject} />
       </Switch>
     </div>
     </BrowserRouter>
---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class CreateProject extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+            title:'',
+            content:''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = (event)=> {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <div className='container'>
+              <form onSubmit={this.handleSubmit} className='white'>
+                  <h5 className='grey-text text-darken-3'>Create A Project !</h5>
+                  <div className='input-field'>
+                    <label htmlFor='title'>Project Title</label>
+                    <input type='text' id='title' onChange={this.handleChange}/>
+                  </div>
+                  <div className='input-field'>
+                    <label htmlFor='content'>Project Content</label>
+                    <textarea id='content' className='materialize-textarea' onChange={this.handleChange}></textarea>
+                  </div>
+                  <div className='input-field'>
+                    <button className='btn pink lighten-1 z-depth-0'>Create</button>
+                  </div>
+
+              </form>
+            </div>
+        )
+    }
+}
+
+export default CreateProject;
